bae4dce189a3d0437ab9181d1788cc46d361c000
Muhammad Umer <umer44447@gmail.com>
2022-06-21 10:29:35 -0700

    Server

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js'
+import dotenv from 'dotenv'
+
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+app.get('/', (req,res) => {
+  res.send('Hello to the Backend')
+})
+
+const PORT = process.env.PORT|| 4000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+// mongoose.set('useFindAndModify', false);
